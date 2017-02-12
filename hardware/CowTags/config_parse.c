8c6aad32c898949c2ed8ac2c3e68f7e841e7b26f
Steven Hall <Hallzy.18@gmail.com>
2017-02-11 18:58:55 -0800

    Configuration Extraction improved - no memory leaks
    * Fixed Memory issues
    * Removed the dependency of malloc and free
    * slightly changed the way that the function is used
      * Now the functions return the return code
      * returned value of the variable is now returned as a function
        argument that was passed by reference

--- hardware/CowTags/config_parse.c
+++ hardware/CowTags/config_parse.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char* VarFromConfigStrExplicitFilename(char const* filename,
+int VarFromConfigStrExplicitFilename(char const* filename,
                                        char const* searchString,
-                                       int* ret) {
+                                       char* output) {
   // Copy searchString to a mutable variable
   char search[MAX_SIZE_SEARCH_STRING];
   strcpy(search, searchString);
@@ -15,45 +15,46 @@ char* VarFromConfigStrExplicitFilename(char const* filename,
 
   FILE *file = fopen(filename, "r");
   if (file != NULL) {
-    char* line_p = (char *)malloc(MAX_SIZE_OF_LINE);
-    char line[MAX_SIZE_OF_LINE];
     // Iterate through every line of the config file
-    while (fgets (line, MAX_SIZE_OF_LINE, file) != NULL) {
+    while (fgets (output, MAX_SIZE_OF_LINE, file) != NULL) {
       // if we get a match, close the file, set return to 1 and copy the line to
       // a char* type
-      if (strstr(line, search) != NULL) {
+      if (strstr(output, search) != NULL) {
         fclose(file);
 
         // Now line_p is the same as line but without all the stuff before the
         // "="
-        strcpy(line_p, line);
-        line_p = strstr(line_p, "=");
+        strcpy(output, strstr(output, "="));
 
         // Remove the equals (this moves the pointer forward 1 character,
         // effectively eliminating the first character in the string)
-        line_p++;
+        strcpy(output, output+1);
 
         // Remove the newline character at the end
-        if (line_p[strlen(line_p)-1] == '\n') {
+        if (output[strlen(output)-1] == '\n') {
           // 0 is the null terminator
-          line_p[strlen(line_p)-1] = 0;
+          output[strlen(output)-1] = 0;
         }
 
-        *ret = 1;
-        return line_p;
+        return 1;
       }
     }
   }
-  *ret = 0;
-  return (char *)"";
+  fclose(file);
+  return 0;
 }
 
-char* varFromConfigStr(char const* searchString, int* ret) {
-  return varFromConfigStrExplicitFilename("../global.conf", searchString, ret);
+int varFromConfigStr(char const* searchString, char* output) {
+  return VarFromConfigStrExplicitFilename("../global.conf", searchString, output);
 }
 
-int varFromConfigInt(char const* searchString, int* ret) {
-  char *var = varFromConfigStr(searchString, ret);
+int varFromConfigInt(char const* searchString, int* output) {
+  char out[MAX_SIZE_OF_LINE];
+  int ret;
 
-  return atoi(var);
+  ret = varFromConfigStr(searchString, out);
+
+  *output = atoi(out);
+
+  return ret;
 }
--- hardware/CowTags/config_parse.h
+++ hardware/CowTags/config_parse.h
@@ -4,11 +4,13 @@
 #define MAX_SIZE_OF_LINE 128
 #define MAX_SIZE_SEARCH_STRING 128
 
-// args: filename, variable to search for, return code
-char* varFromConfigStrExplicitFilename(char const*, char const*, int*);
+// args: filename, variable to search for, returned value
+// Function returns return code
+int varFromConfigStrExplicitFilename(char const*, char const*, char*);
 
-// args: variable to search for, return code
-char* varFromConfigStr(char const*, int*);
+// args: variable to search for, returned value
+// Function returns return code
+int varFromConfigStr(char const*, char*);
 int   varFromConfigInt(char const*, int*);
 
 #endif // CONFIG_PARSE_H

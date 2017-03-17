1620244aa67e9aaf9ee2f8884f9d34ecba57bec9
Steven Hall <Hallzy.18@gmail.com>
2017-03-16 17:13:02 -0700

    Updated the code for writing the timestamp
    Now the code counts the number of bytes to the write point on its own
    (meaning that edits can be made to the file without the possibility of
    breaking it, so long as these edits do not affect the line that we are
    writing to).
    
    The executable name can also be anything now, and the program will
    automatically strip that name off of the path regardless of what it is.

--- add_timestamp_to_config.c
+++ add_timestamp_to_config.c
@@ -11,9 +11,49 @@
 
 #define MAX_PATH 200
 #define EPOCH_TIME_CHARS 15
+#define TESTING_DONT_WRITE_TO_FILE 0
 
 int getCurrentTime() {
   return (int)time(NULL);
+}
+
+// Returns the byte offset to get to the edit point
+int Search_in_File(FILE *fp, char *str) {
+  char current_line[512];
+  int byte_count=0;
+  char c;
+
+  while (fgets(current_line, 512, fp) != NULL) {
+    if ((strstr(current_line, str)) != NULL) {
+      // String found - Take the current count, and add the length of the search
+      // string to the byte_count (to get the byte_count to the end of the line)
+      byte_count+=strlen(str)+1;
+      break;
+    }
+    // Count the number of bytes in the line if the line does not match
+    for (int i = 0; i < sizeof(current_line); i++) {
+      // If the current character is a newline, that is the end of the line, so
+      // the current index+1 is the number of characters on that line
+      if (current_line[i] == '\n') {
+        byte_count+=i+1;
+        break;
+      }
+    }
+  }
+  return byte_count;
+}
+
+int removeExecutableFilenameFromString(char *str) {
+  // Start checking for the first forward slash from the end of the string
+  for (int i = strlen(str)-1; i >= 0; i--) {
+    if (str[i] == '/') {
+      // When we find the forward slash, replace it with a null character (ie,
+      // end the string here)
+      str[i] = 0;
+      return 1;
+    }
+  }
+  return 0;
 }
 
 int main() {
@@ -26,25 +66,34 @@ int main() {
   readlink("/proc/self/exe", path, MAX_PATH);
 #endif
 
-  // Remove "/config" from the end of the path
-  path[strlen(path)-7]=0;
+  // Remove this executable name from the end of the path
+  // path is returned as the updated string from this function
+  int ret = removeExecutableFilenameFromString(path);
+  if (!ret) {
+    printf("Error occurred trying to extract filepath");
+    return 1;
+  }
   // Append the rest of the path to the config file
   strcat(path, "/hardware/CowTags/global_cfg.h");
 
   // Open config file for writing
   FILE *fp = fopen(path, "r+");
 
-  // Start editing at the 242th character
-  fseek (fp, 242, SEEK_SET);
+  int byte_count = Search_in_File(fp, "#define TIMESTAMP_AT_BUILDTIME");
+  // Start editing at the byte_count character:
+  // C works by needing to count the number of bytes from the start of the file
+  // to the point you want to edit. byte_count is that number of bytes
+  fseek (fp, byte_count, SEEK_SET);
 
   // Convert the number received from getCurrentTime() to a string with snprintf
   // That string gets saved into "current_time_str"
   char current_time_str[EPOCH_TIME_CHARS];
   snprintf(current_time_str, EPOCH_TIME_CHARS, "%d", getCurrentTime());
 
-  // Write the number to the file
-  /* fprintf(fp, current_time_str); */
-  fputs(current_time_str, fp);
+  if (!TESTING_DONT_WRITE_TO_FILE) {
+    // Write the number to the file
+    fputs(current_time_str, fp);
+  }
 
   fclose(fp);
   return 0;

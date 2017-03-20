5cf0a8b772e21efda3d61ca2efb8fe2d9463af5d
Steven Hall <Hallzy.18@gmail.com>
2017-03-20 14:12:56 -0700

    Pass the file path to the c program instead

--- add_timestamp_to_config.c
+++ add_timestamp_to_config.c
@@ -5,15 +5,15 @@
 
 // OS specific libraries
 //#ifdef _WIN32
-#ifdef  __gnu_linux__
-  #include <unistd.h>
-#elif __WIN32
-  #include <Windows.h>
-  #include <Winbase.h>
-  #include <direct.h>
-#endif
+/* #ifdef  __gnu_linux__ */
+/*   #include <unistd.h> */
+/* #elif __WIN32 */
+/*   #include <Windows.h> */
+/*   #include <Winbase.h> */
+/*   #include <direct.h> */
+/* #endif */
 
-#define MAX_CFG_FILE_PATH 200
+/* #define MAX_CFG_FILE_PATH 200 */
 #define EPOCH_TIME_CHARS 15
 #define TESTING_DONT_WRITE_TO_FILE 0
 
@@ -51,52 +51,60 @@ int Search_in_File(FILE *fp, char *str) {
     }
   }
 #ifdef __WIN32
+  // Need to do this for windows, because for some reason windows thought it
+  // would be a good idea to use 2 characters for a newline instead of 1 like
+  // everybody else. So for every line above the matched line we need to add 1
+  // more character.
   return byte_count + current_line_number - 1;
 #else
   return byte_count;
 #endif
 }
 
-#if  __gnu_linux__
-int removeExecutableFilenameFromString(char *str) {
-  // Start checking for the first forward slash from the end of the string
-  for (int i = strlen(str)-1; i >= 0; i--) {
-    if (str[i] == '/') {
-      // When we find the forward slash, replace it with a null character (ie,
-      // end the string here)
-      str[i] = 0;
-      return 1;
-    }
-  }
-  return 0;
-}
-#endif
+/* #if  __gnu_linux__ */
+/* int removeExecutableFilenameFromString(char *str) { */
+/*   // Start checking for the first forward slash from the end of the string */
+/*   for (int i = strlen(str)-1; i >= 0; i--) { */
+/*     if (str[i] == '/') { */
+/*       // When we find the forward slash, replace it with a null character (ie, */
+/*       // end the string here) */
+/*       str[i] = 0; */
+/*       return 1; */
+/*     } */
+/*   } */
+/*   return 0; */
+/* } */
+/* #endif */
 
-int main() {
-  char path[MAX_CFG_FILE_PATH];
-
-  // Get the path of the config executable
-#ifdef __WIN32
-  getcwd(path, MAX_CFG_FILE_PATH);
-#elif  __gnu_linux__
-  readlink("/proc/self/exe", path, MAX_CFG_FILE_PATH);
-
-  // Remove this executable name from the end of the path
-  // path is returned as the updated string from this function
-  int ret = removeExecutableFilenameFromString(path);
-  if (!ret) {
-    printf("Error occurred trying to extract filepath");
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("Expects the path to the config file as argument\n");
     return 1;
   }
-#endif
+  /* char path[MAX_CFG_FILE_PATH]; */
+
+  // Get the path of the config executable
+/* #ifdef __WIN32 */
+/*   getcwd(path, MAX_CFG_FILE_PATH); */
+/* #elif  __gnu_linux__ */
+/*   readlink("/proc/self/exe", path, MAX_CFG_FILE_PATH); */
+
+/*   // Remove this executable name from the end of the path */
+/*   // path is returned as the updated string from this function */
+/*   int ret = removeExecutableFilenameFromString(path); */
+/*   if (!ret) { */
+/*     printf("Error occurred trying to extract filepath"); */
+/*     return 1; */
+/*   } */
+/* #endif */
 
   // Append the rest of the path to the config file
-  strcat(path, "/hardware/CowTags/global_cfg.h");
+  /* strcat(path, "/hardware/CowTags/global_cfg.h"); */
 
-  printf("config path: %s\n", path);
+  printf("config path: %s\n", argv[1]);
 
   // Open config file for writing
-  FILE *fp = fopen(path, "r+");
+  FILE *fp = fopen(argv[1], "r+");
 
   int byte_count = Search_in_File(fp, "#define TIMESTAMP_AT_BUILDTIME");
   // Start editing at the byte_count character:

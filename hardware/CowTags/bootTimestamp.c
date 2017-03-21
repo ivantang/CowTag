b476f46a5c506b53ececc153475ad29e362dca56
Steven Hall <Hallzy.18@gmail.com>
2017-03-21 11:20:14 -0700

    Moved the timestamp update to pre build because that makes more sense

--- hardware/CowTags/bootTimestamp.c
+++ hardware/CowTags/bootTimestamp.c
@@ -5,19 +5,24 @@
 #include <stdio.h>
 #include <xdc/runtime/Types.h>
 
+// The timestamp returned by Timestamp_get32() / frequency is really just the
+// number of seconds that the tag has been up for. The timestamp is not accurate
+// to real world time
+
+// TIMESTAMP_AT_BUILDTIME is a value defined in the config file, and is set
+// automatically after the project builds to be the current timestamp of the
+// computer that is building the project, thus adding the uptime to this will
+// give us a semi-accurate timestamp to use for our packets
 uint32_t TrueTimestamp() {
-  /* uint32_t uptime = Timestamp_get32() - boot_timestamp; */
-	Types_FreqHz frequency;
-	Timestamp_getFreq(&frequency);
-  uint32_t current = Timestamp_get32() / (frequency.hi << 8 | frequency.lo);
+  Types_FreqHz frequency;
+  Timestamp_getFreq(&frequency);
+  uint32_t current_stamp = Timestamp_get32() / (frequency.hi << 8 | frequency.lo);
+
+	// boot_timestamp is defined in bootTimestamp.h, and is the first thing that
+	// is set in the main() function
   uint32_t uptime = current - boot_timestamp;
   uint32_t ret = TIMESTAMP_AT_BUILDTIME + uptime;
-  /* return TIMESTAMP_AT_BUILDTIME + uptime; */
 
-  printf("current = %i\n", current);
-  printf("boot_timestamp = %i\n", boot_timestamp);
-  printf("uptime = %i\n", uptime);
-  printf("TIMESTAMP_AT_BUILDTIME = %i\n", TIMESTAMP_AT_BUILDTIME);
-  printf("ret = %i\n", ret);
+  printf("Timestamp = %i\n", ret);
   return ret;
 }

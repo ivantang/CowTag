f9b4adc8b9a95400d17e93d0a1a18dadfff70c47
Steven Hall <Hallzy.18@gmail.com>
2017-04-04 15:18:00 -0700

    Added more comments and example to config_parse.h

--- hardware/CowTags/config_parse.h
+++ hardware/CowTags/config_parse.h
@@ -9,7 +9,15 @@
 int varFromConfigStrExplicitFilename(char const*, char const*, char*);
 
 // args: variable to search for, returned value
-// Function returns return code
+// Function returns return code 1 for success, 0 for failure
+// Example Usage (assuming cfg_variable is a variable defined in global.cfg):
+// int var;
+// if (varFromConfigInt("cfg_variable", &var)) {
+//   var was set
+// }
+// else {
+//   var was not set
+// }
 int varFromConfigStr(char const*, char*);
 int varFromConfigInt(char const*, int*);
 
